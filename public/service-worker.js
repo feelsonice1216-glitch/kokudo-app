ee6c0c7583f1a27894feb65e988983b2da6ccd89
feelsonice1216-glitch <feel.so.nice.1216@gmail.com>
2026-08-02 17:28:23 +0900

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'kokudo-sticker-cache-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
